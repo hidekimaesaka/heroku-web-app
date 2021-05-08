9c30bf82bdbdb1c3c8f83a78b290ad8a5b99836c
Hideki Maesaka <lucasmsk11@gmail.com>
2021-05-08 00:34:21 -0300

    second attempt

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-import os
 import flask from Flask
-from flask_cors import CORS
 
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
     return "<h1> Hello world </h1>"
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
 
 
 if __name__ == "__main__":
