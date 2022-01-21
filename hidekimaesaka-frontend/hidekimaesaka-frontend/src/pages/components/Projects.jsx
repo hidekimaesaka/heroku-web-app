e3b61e4b58f75820d0c7b8e66f530069aa4f1b39
Hideki Maesaka <lucasmsk11@gmail.com>
2022-01-21 11:32:05 -0300

    Ajuste na aparencia da pagina de projetos

--- hidekimaesaka-frontend/hidekimaesaka-frontend/src/pages/components/Projects.jsx
+++ hidekimaesaka-frontend/hidekimaesaka-frontend/src/pages/components/Projects.jsx
@@ -5,7 +5,7 @@ import "../css/Projects.css"
 export default function Projects(){
     return(
         <div className="main-projects">
-            <a className="p1" href="app-invest.vercel.app/">App-Invest</a>
+            <a className="p1" href="https://app-invest.vercel.app/">App-Invest</a>
             <div className="p2">Project 2</div>
             <div className="p3">Project 3</div>
             <div className="p4">Project 4</div>
