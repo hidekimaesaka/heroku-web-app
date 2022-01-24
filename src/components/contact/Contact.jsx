8ed3e1452ac8d5a8efbac5d0fe12429f5c78e457
Hideki Maesaka <lucasmsk11@gmail.com>
2022-01-24 16:43:31 -0300

    adição de email-box na aba contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,7 @@
 import "./contact.scss";
-import { useState } from "react";
 
 export default function Contact() {
 
-  const [message, setMessage] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true)
-  }
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -16,11 +9,13 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="Email"></input>
-          <textarea placeholder="Message"></textarea>
+        <form action="https://api.staticforms.xyz/submit" method="post">
+        <input type="hidden" name="accessKey" value="8463691e-a088-4024-89ef-acfd745509f0"></input>
+        <input type="hidden" name="redirectTo" value="https://hidekimsk.com/#home"></input>
+        <input name="name" type="text" placeholder="Name"></input>
+          <input name="email" type="email" placeholder="Email"></input>
+          <textarea name="message" placeholder="Message"></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks!</span>}
         </form>
       </div>
     </div>
