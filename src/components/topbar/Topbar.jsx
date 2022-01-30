60ac8e89f9bcffd6a7d68215de04113b8bf28bae
Hideki Maesaka <lucasmsk11@gmail.com>
2022-01-29 21:43:48 -0300

    Adiciona download de currículo em TopBar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,17 @@
 import "./topbar.scss";
-import PersonIcon from '@mui/icons-material/Person';
-import MailIcon from '@mui/icons-material/Mail';
+import PersonIcon from "@mui/icons-material/Person";
+import MailIcon from "@mui/icons-material/Mail";
+import DownloadIcon from "@mui/icons-material/Download";
+import resume from "../resume/LUCAS_HIDEKI_MAESAKA_CV.pdf";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">hidekimsk</a>
+          <a href="#intro" className="logo">
+            hidekimsk
+          </a>
           <div className="itemContainer">
             <PersonIcon className="icon"></PersonIcon>
             <span>+(55) (11) 97655-4986 </span>
@@ -15,6 +19,12 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           <div className="itemContainer">
             <MailIcon className="icon"></MailIcon>
             <span>lucasmsk11@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <DownloadIcon className="icon"></DownloadIcon>
+            <a href={resume} download="CV_Lucas_Hideki_Maesaka.pdf">
+              <span>Baixe meu currículo!</span>
+            </a>
           </div>
         </div>
         <div className="right">
